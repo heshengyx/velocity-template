cd24e29ffa1efb5a2a53df01b6fa16fe53694e26
hes <heshengyx@qq.com>
2015-12-01 20:01:46 +0800

    1

--- src/main/java/com/velocity/template/enums/AttributeTypeEnum.java
+++ src/main/java/com/velocity/template/enums/AttributeTypeEnum.java
@@ -1,23 +1,38 @@
 package com.velocity.template.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum AttributeTypeEnum {
 
-	STRING("String", "VARCHAR"),
-	DATE("Date", "TIMESTAMP");
-	
+	STRING("String", "VARCHAR"), DATE("Date", "TIMESTAMP");
+
 	private String text;
-	private String jdbcType; 
-	
+	private String jdbcType;
+
 	private AttributeTypeEnum(String text, String jdbcType) {
 		this.text = text;
 		this.jdbcType = jdbcType;
 	}
-	
+
 	public String getJdbcType() {
 		return jdbcType;
 	}
+
 	public String getText() {
 		return text;
 	}
 
+	private static Map<String, String> types = new HashMap<String, String>();
+	static {
+		AttributeTypeEnum[] enums = AttributeTypeEnum.values();
+		for (AttributeTypeEnum attributeTypeEnum : enums) {
+			types.put(attributeTypeEnum.getText(),
+					attributeTypeEnum.getJdbcType());
+		}
+	}
+
+	public String getJdbcType(String text) {
+		return types.get(text);
+	}
 }
